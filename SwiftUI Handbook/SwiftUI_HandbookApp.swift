8d8cbc23142780c929c752280898d46a329db021
ch8ser <we@thech8sers.com>
2022-07-11 00:07:30 -0400

    Initial Commit

---
+++ SwiftUI Handbook/SwiftUI_HandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HandbookApp.swift
+//  SwiftUI Handbook
+//
+//  Created by The Ch8sers Inc. on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
